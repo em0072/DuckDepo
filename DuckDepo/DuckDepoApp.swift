ff893cac2cbbaa23f70e525a94cb3d9f48babb84
Evgeny Mitko <em0072@gmail.com>
2021-10-25 12:49:11 +0200

    Initial Commit

---
+++ DuckDepo/DuckDepoApp.swift
@@ -0,0 +1,20 @@
+//
+//  DuckDepoApp.swift
+//  DuckDepo
+//
+//  Created by Evgeny Mitko on 25/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct DuckDepoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
